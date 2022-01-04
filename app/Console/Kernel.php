8fe9c59b413f1523b8aa08bb9c7a9fe78c36b609
obabeyi <43567012+obabeyi@users.noreply.github.com>
2022-01-04 18:23:04 +0300

    css düzenleme

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -44,7 +44,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('demo:cron')->everyMinute();
        // $schedule->command('namaz:cron')->monthlyOn(15, '00:00');
-        //   $schedule->command('namaz:cron')->everyMinute();
+//           $schedule->command('namaz:cron')->everyMinute();
         ///php artisan schedule:run ile tek sefer çalışıyor
         ///php artisan schedule:work ile devamlı çalışıyor
     }
