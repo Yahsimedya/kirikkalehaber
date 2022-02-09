b98efce29ffbd7298e177c34edbce7234686ef8a
onur-akdogan <onurakdogan7106@gmail.com>
2022-02-09 17:33:22 +0300

    Update add_photo.blade.php

--- resources/views/backend/galery/add_photo.blade.php
+++ resources/views/backend/galery/add_photo.blade.php
@@ -19,9 +19,7 @@
 
 
             <div class="card-body">
-                <p class="mb-3">Example of a <code>multiple</code> file uploader based on <code>Dropzone.js</code> library. Dropzone.js is a light weight JavaScript library that turns an HTML element into a dropzone. This means that a user can drag and drop a file onto it, and the file gets uploaded to the server via AJAX. By default Dropzone is a multiple file uploader, so this example is a basic setup. Uploading process runs automatically and image thumbnail previews are shown right after file selection.</p>
 
-                <p class="font-weight-semibold">Multiple file upload example:</p>
                 <div class="form-group pt-4 pb-4">
                     <a class="btn btn-success float-right" href="{{route('galery.detail',$photocategory->photocategory_id)}}"><i class="icon-stack-plus" aria-hidden="true"></i>
                         Fotoları Gör
@@ -30,7 +28,10 @@
                     @csrf
                     <input type="hidden" name="photocategory_id" id="dropzone_remove" value="{{$photocategory->photocategory_id}}">
 
-                    <div class="dz-default dz-message"><button class="dz-button" type="button">Drop files to upload <span>or CLICK</span></button></div>
+                    <div class="dz-default dz-message">
+                        <button class="dz-button" type="button">
+                            Fotoğrafları sürükle yada tıkla
+                        </button></div>
                 </form>
             </div>
         </div>
@@ -41,7 +42,7 @@
 
         $("#dropzone_multiple").dropzone({
             paramName: "file", // The name that will be used to transfer the file
-            dictDefaultMessage: 'Drop files to upload <span>or CLICK</span>',
+            dictDefaultMessage: 'Fotoğrafları sürükle yada tıkla',
             maxFilesize: 1, // MB
             headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
 
